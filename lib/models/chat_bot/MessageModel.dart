d6c7e708206753dc535ddda3b193b1f3c29a5890
Matovu Ronald <maronwhite6@gmail.com>
2024-10-10 12:32:46 +0300

    Refactor MessageModel to include optional expert information

--- lib/models/chat_bot/MessageModel.dart
+++ lib/models/chat_bot/MessageModel.dart
@@ -3,6 +3,9 @@ class Message {
   final String sender;
   final String message;
   final String messageType;
+  final String? respondedBy;
+  final Expert?
+      expert; // Nullable expert object if the response was by an expert
   final DateTime createdAt;
 
   Message({
@@ -10,6 +13,8 @@ class Message {
     required this.sender,
     required this.message,
     required this.messageType,
+    this.respondedBy,
+    this.expert,
     required this.createdAt,
   });
 
@@ -19,7 +24,28 @@ class Message {
       sender: json['sender'],
       message: json['message'],
       messageType: json['message_type'],
+      respondedBy: json['responded_by'],
+      expert: json['expert'] != null
+          ? Expert.fromJson(json['expert'])
+          : null, // Parsing 'expert' if available
       createdAt: DateTime.parse(json['created_at']),
+    );
+  }
+}
+
+class Expert {
+  final int id;
+  final String name;
+
+  Expert({
+    required this.id,
+    required this.name,
+  });
+
+  factory Expert.fromJson(Map<String, dynamic> json) {
+    return Expert(
+      id: json['id'],
+      name: json['name'],
     );
   }
 }
